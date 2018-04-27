d0f6db54cd03d05d102ef93b92456bb3793d57c4
ACE <450685157@qq.com>
2018-04-27 19:46:32 +0800

    Initial commit

---
+++ src/main/java/com/zolarrobot/newlib/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.zolarrobot.newlib;
+
+/***
+ *@methodAnnotation:
+ *@Author: $author$
+ *@Date  :  $date$
+ */
+public class HelloWorld {
+    public void hello(){
+        System.out.println("hello");
+    }
+}
